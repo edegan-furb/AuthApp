24717f0570e412f17d5ec7a1f162fff3c6946573
edegan <eduardord324@gmail.com>
2023-11-27 17:08:10 -0300

    Class 172 - 178

--- constants/styles.js
+++ constants/styles.js
@@ -1,7 +1,7 @@
 export const Colors = {
-  primary100: '#f9beda',
-  primary500: '#c30b64',
-  primary800: '#610440',
-  error100: '#fcdcbf',
-  error500: '#f37c13',
+  primary100: '#e0e7ff',
+  primary500: '#6366f1',
+  primary800: '#1e1b4b',
+  error100: '#fee2e2',
+  error500: '#ef4444',
 }
