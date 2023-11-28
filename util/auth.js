5dfbf7810f2e265a4ac8489285b484f5dc7a9e7a
Eduardo Degan <edegan@furb.br>
2023-11-28 02:16:15 -0300

    Class 181 - 188

--- util/auth.js
+++ util/auth.js
@@ -6,17 +6,21 @@ async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
   const response = await axios.post(url, {
+    //displayName: "teste",
     email: email,
     password: password,
     returnSecureToken: true,
   });
-  console.log(response.data);
+  //console.log(response.data);
+
+  const token = response.data.idToken;
+  return token;
 }
 
-export async function createUser(email, password) {
-  await authenticate("signUp", email, password);
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
 }
 
-export async function login(email, password) {
-  await authenticate("signInWithPassword", email, password);
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
