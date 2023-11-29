572e47d583c4654b7fd09fd501294e172cce516b
Eduardo Rebelo Degan <edegan@furb.br>
2023-11-29 15:44:04 -0300

    Update auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyBnet9ItWPZWL1f4LvTKD61XYWDoIM8-9A";
+const API_KEY = "";
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
